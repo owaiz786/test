760ed591b3c528f04dc2a878af1e098851881098
owaiz786 <owaizsajida@gmail.com>
2025-04-23 15:54:42 +0530

    update

--- database.py
+++ database.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://glucose_values_user:b3TusfqGRWK2aVMwGfSL9iSttAYZxeoQ@dpg-d047p6a4d50c739tck5g-a/glucose_values"
+DATABASE_URL = "postgresql://glucose_values_user:your_password@your_host:5432/glucose_values"
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# ✅ This is what FastAPI is trying to import
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
